a3f04549eb804274a284d0d2337092845ac435d4
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2021-11-11 09:14:11 -0300

    Listagem funcionando

--- atividade_crud/listagem/index.php
+++ atividade_crud/listagem/index.php
@@ -1,47 +1,79 @@
 <?php
-    include('../componentes/header.php');
+include('../componentes/header.php');
+
+require('../database/conexao.php');
+
+$sql = "SELECT * FROM tbl_pessoa";
+
+$pessoas = mysqli_query($conexao, $sql);
+
+$resultado = mysqli_fetch_array($pessoas);
 ?>
 
 <div class="container">
 
-    <br/>
-    
+    <br />
+
     <table class="table table-bordered">
 
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Sobrenome</th>
-            <th>E-mail</th>
-            <th>Celular</th>
-            <th>Ações</th>
-        </tr>
-    </thead>
-
-    <tbody>
+        <thead>
             <tr>
-                <th>1</th>
-                <th>TESTE DE NOME</th>
-                <th>TESTE DE SOBRENOME</th>
-                <th>TESTE DE EMAIL</th>
-                <th>TESTE DE CELULAR</th>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Sobrenome</th>
+                <th>E-mail</th>
+                <th>Celular</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+
+        <tbody>
+
+            <tr>
+
+                <th><?php echo $resultado["cod_pessoa"];  ?></th>
+                <th><?php echo $resultado["nome"];  ?></th>
+                <th><?php echo $resultado["sobrenome"];  ?></th>
+                <th><?php echo $resultado["email"];  ?></th>
+                <th><?php echo $resultado["celular"];  ?></th>
+
                 <th>
                     <button class="btn btn-warning">Editar</button>
-
                     <form action="" method="post" style="display: inline;">
                         <input type="hidden" name="id" value="">
                         <button class="btn btn-danger">Excluir</button>
                     </form>
-                    
                 </th>
             </tr>
-    </tbody>
+
+            <?php
+            while ($resultado = mysqli_fetch_array($pessoas)) {
+            ?>
+                <th><?php echo $resultado["cod_pessoa"];  ?></th>
+                <th><?php echo $resultado["nome"];  ?></th>
+                <th><?php echo $resultado["sobrenome"];  ?></th>
+                <th><?php echo $resultado["email"];  ?></th>
+                <th><?php echo $resultado["celular"];  ?></th>
+
+                <th>
+                    <button class="btn btn-warning">Editar</button>
+                    <form action="" method="post" style="display: inline;">
+                        <input type="hidden" name="id" value="">
+                        <button class="btn btn-danger">Excluir</button>
+                    </form>
+                </th>
+                </tr>
+
+            <?php
+            }
+            ?>
+
+        </tbody>
 
     </table>
 
 </div>
 
 <?php
-    include('../componentes/footer.php');
+include('../componentes/footer.php');
 ?>
