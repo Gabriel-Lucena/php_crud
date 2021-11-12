c53cb989661d2be5afe300e94032271b736e3435
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2021-11-12 10:26:18 -0300

    Cadastro com login

--- atividade_crud/cadastro/index.php
+++ atividade_crud/cadastro/index.php
@@ -1,31 +1,39 @@
 <?php
-    include('../componentes/header.php');
+
+session_start();
+
+include('../componentes/header.php');
+require('../login/verificarLogin.php');
+
+
+verificarLogin($_SESSION["id"]) == false ? header('location: ../login') : null;
+
 ?>
 
 
-    <div class="container">
-        <hr>
-        <div class="card">
-            <div class="card-header">
-                <h2>Cadastro</h2>
-            </div>
-            <div class="card-body">
-                <form method="post" action="./acao.php">
-                    <input class="form-control" type="text" placeholder="Digite o nome" name="nome" id="nome">
-                    <br />
-                    <input class="form-control" type="text" placeholder="Digite o sobrenome" name="sobrenome" id="sobrenome">
-                    <br />
-                    <input class="form-control" type="text" placeholder="Digite o email" name="email" id="email">
-                    <br />
-                    <input class="form-control" type="text" placeholder="Digite celular" name="celular" id="celular">
-                    <br />
-                    <button class="btn btn-success">CADASTRAR</button>
-                </form>
-            </div>
+<div class="container">
+    <hr>
+    <div class="card">
+        <div class="card-header">
+            <h2>Cadastro</h2>
+        </div>
+        <div class="card-body">
+            <form method="post" action="./acao.php">
+                <input class="form-control" type="text" placeholder="Digite o nome" name="nome" id="nome">
+                <br />
+                <input class="form-control" type="text" placeholder="Digite o sobrenome" name="sobrenome" id="sobrenome">
+                <br />
+                <input class="form-control" type="text" placeholder="Digite o email" name="email" id="email">
+                <br />
+                <input class="form-control" type="text" placeholder="Digite celular" name="celular" id="celular">
+                <br />
+                <button class="btn btn-success">CADASTRAR</button>
+            </form>
         </div>
     </div>
+</div>
 
 
 <?php
-    include('../componentes/footer.php');
+include('../componentes/footer.php');
 ?>
